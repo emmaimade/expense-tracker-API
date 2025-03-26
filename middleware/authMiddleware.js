2ab14db131be7897f6d6e537a480b698c1c0b0bc
emmaimade <emmaimade14@gmail.com>
2025-03-26 22:10:54 +0100

    Add middleware

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,25 @@
+import jwt from "jsonwebtoken";
+
+const authMiddleware = async (req, res, next) => {
+    try {
+        let token = req.headers.authorization;
+
+        if (!token) {
+            return res.status(401).json({ error: "Access Denied" });
+        }
+
+        token = token.split(" ")[1];
+        jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+            if (err) {
+                return res.status(403).json({ error: "Invalid Token" });
+            }
+
+            req.user = decoded;
+            next();
+        })
+    } catch (error) {
+        return res.status(403).json({ error: "Invalid or expired token"})
+    }
+}
+
+module.exports = authMiddleware;
