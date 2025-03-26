5b1bff2830cc20afe99e5397f99901d83b7d9e74
emmaimade <emmaimade14@gmail.com>
2025-03-26 22:22:56 +0100

    Add Expense feature

---
+++ controllers/expense.js
@@ -0,0 +1,30 @@
+import Expense from "../models/Expense.js";
+
+const addExpense = async (req, res) => {
+    const { amount, category, description } = req.body;
+
+    if (!amount || !category || !description) {
+        return res.status(400).json({ error: "Please fill all the fields" });
+    }
+
+    if (isNaN(amount) || amount <= 0) {
+        return res.status(400).json({ error: "Amount must be a positive number" });
+    }
+
+    try {
+        const expense = await Expense.create({
+            userId: req.user.id,
+            amount,
+            category,
+            description
+        });
+
+        res.status(201).json(expense);
+    } catch (error) {
+        return res.status(500).json({ error: "Internal Server Error" });
+    }
+}
+
+module.exports = {
+    addExpense
+}
