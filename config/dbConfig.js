34503620020d998ac7199841b333e1b296f8a144
emmaimade <emmaimade14@gmail.com>
2025-03-29 07:09:24 +0100

    Update dbConfig

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,14 +1,14 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-    mongoose.connect(process.env.DB_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }).then(() => {
-        console.log("Connected to MongoDB");
-    }).catch((err) => {
-        console.log("Failed to connect to MongoDB", err);
+  mongoose
+    .connect(process.env.DB_URI)
+    .then(() => {
+      console.log("Connected to MongoDB");
     })
-}
+    .catch((err) => {
+      console.log("Failed to connect to MongoDB", err);
+    });
+};
 
 export default connectDB;
