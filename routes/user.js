bf7ebffe423ba6b96db2103b30f07b283ac7199a
emmaimade <emmaimade14@gmail.com>
2025-03-29 07:18:44 +0100

    Remove authMiddleware

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,10 @@
 import express from "express";
 
 import { registerUser, loginUser } from "../controllers/user.js";
-import authMiddleware from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
-router.post("/login", authMiddleware, loginUser);
+router.post("/login", loginUser);
 
 export default router;
